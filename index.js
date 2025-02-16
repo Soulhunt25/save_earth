7adaaba0b913f96c4f11eecbeec58183f6cad53d
Kanishka99999 <kanishkapatel72@gmail.com>
2025-02-16 17:43:16 +0530

    added complete code of login

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 let login_email ="a@gmail.com"
 
 let login_pass = "12345"
+
+let input_email ="a@gmail.com"
+
+let input_pass = "12345"
+
+
+
+if(login_email == input_email && login_pass ==input_pass){
+
+console.log("login successfull");
+
+} else {
+
+console.log("invaild");
+
+}
